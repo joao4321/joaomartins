73f0f368a247409a87c1cdc8ec4d75f39202d6ac
joao4321 <89591695+joao4321@users.noreply.github.com>
2023-06-13 23:56:53 -0300

    Update ativ5.js

--- ativ5.js
+++ ativ5.js
@@ -1,65 +1,112 @@
-<!DOCTYPE html>
-<html lang="en">
+import  React ,  {  useState  }  from  'react' ;
+importar  './App.css' ;
 
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js"></script>
-	<script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
-	<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
-	<script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
-	<style>
-		*{
-			background-color: #bdbebd;
-		}
-		p {
-			font-size: 50px;
-			font-weight: bold;
-			color: #0000ff;
-			text-align: center;
-			margin-top: 20%;
-		}
-	</style>
-</head>
+função  Aplicativo ( )  {
+  const  [ displayValue ,  setDisplayValue ]  =  useState ( '0' ) ;
+  const  [ firstOperand ,  setFirstOperand ]  =  useState ( null ) ;
+  const  [ operador ,  setOperator ]  =  useState ( null ) ;
+  const  [ waitingForSecondOperand ,  setWaitingForSecondOperand ]  =  useState ( false ) ;
 
-<body>
-	<div id="root"></div>
+  const  handleDigitClick  =  ( dígito )  =>  {
+    if  ( waitingForSecondOperand )  {
+      setDisplayValue ( digit.toString ( ) ) ; _ _
+      setWaitingForSecondOperand ( false ) ;
+    }  senão  {
+      setDisplayValue ( displayValue  ===  '0' ? digit . toString ( ) : displayValue  +  digit ) ;
+    }
+  } ;
 
-	<script type="text/babel">
-		class Frase extends React.Component {
-			constructor(props) {
-				super(props);
-				this.state = {
-					texto: ""
-				};
-				this.frase = "bem vindo a fatec!!!";
-				this.timer = null;
-			}
+  const  handleDecimalClick  =  ( )  =>  {
+    if  ( waitingForSecondOperand )  {
+      setDisplayValue ( '0.' ) ;
+      setWaitingForSecondOperand ( false ) ;
+    }  else  if  ( displayValue . indexOf ( '.' )  ===  - 1 )  {
+      setDisplayValue ( displayValue  +  '.' ) ;
+    }
+  } ;
 
-			componentDidMount() {
-				let i = 0;
-				this.timer = setInterval(() => {
-					if (i < this.frase.length) {
-						this.setState({ texto: this.state.texto + this.frase.charAt(i) });
-						i++;
-					} else {
-						clearInterval(this.timer);
-					}
-				}, 100);
-			}
+  const  handleOperatorClick  =  ( selectedOperator )  =>  {
+    const  inputValue  =  parseFloat ( displayValue ) ;
 
-			componentWillUnmount() {
-				clearInterval(this.timer);
-			}
+    if  ( primeiroOperando  ===  null )  {
+      setFirstOperand ( inputValue ) ;
+    }  else  if  ( operador )  {
+       resultado  const =  performCalculation ( ) ;
+      setDisplayValue ( resultado.toString ( ) ) ; _ _
+      setFirstOperand ( resultado ) ;
+    }
 
-			render() {
-				return <p>{this.state.texto}</p>;
-			}
-		}
+    setWaitingForSecondOperand ( true ) ;
+    setOperator ( selectOperator ) ;
+  } ;
 
-		ReactDOM.render(<Frase />, document.getElementById("root"));
-	</script>
-</body>
+  const  performCalculation  =  ( )  =>  {
+    const  inputValue  =  parseFloat ( displayValue ) ;
 
-</html>
+    if  ( operador  ===  '+' )  {
+      return  firstOperand  +  inputValue ;
+    }  else  if  ( operador  ===  '-' )  {
+      return  firstOperand  -  inputValue ;
+    }  else  if  ( operador  ===  '*' )  {
+      return  firstOperand  *  inputValue ;
+    }  else  if  ( operador  ===  '/' )  {
+      return  firstOperand  /  inputValue ;
+    }
+
+    return  ValorEntrada ;
+  } ;
+
+  const  handleEqualClick  =  ( )  =>  {
+    if  ( ! operador )  return ;
+
+     resultado  const =  performCalculation ( ) ;
+    setDisplayValue ( resultado.toString ( ) ) ; _ _
+    setFirstOperand ( resultado ) ;
+    setOperator ( null ) ;
+    setWaitingForSecondOperand ( true ) ;
+  } ;
+
+  const  handleClearClick  =  ( )  =>  {
+    setDisplayValue ( '0' ) ;
+    setFirstOperand ( nulo ) ;
+    setOperator ( null ) ;
+    setWaitingForSecondOperand ( false ) ;
+  } ;
+
+  retornar  (
+    < div  className = "calculadora" >
+      < div  className = "display" > { displayValue } < / div >
+      < div  className = "teclado" >
+        < div  className = "linha" >
+          < botão  className = "btn"  onClick = { ( )  =>  handleClearClick ( ) } > C < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleOperatorClick ( '/' ) } > / < / botão >
+        < / div >
+        < div  className = "linha" >
+          < botão  className = "btn"  onClick = { ( )  =>  handleDigitClick ( 7 ) } > 7 < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleDigitClick ( 8 ) } > 8 < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleDigitClick ( 9 ) } > 9 < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleOperatorClick ( '*' ) } > * < / botão >
+        < / div >
+        < div  className = "linha" >
+          < botão  className = "btn"  onClick = { ( )  =>  handleDigitClick ( 4 ) } > 4 < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleDigitClick ( 5 ) } > 5 < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleDigitClick ( 6 ) } > 6 < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleOperatorClick ( '-' ) } > - < / botão >
+        < / div >
+        < div  className = "linha" >
+          < botão  className = "btn"  onClick = { ( )  =>  handleDigitClick ( 1 ) } > 1 < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleDigitClick ( 2 ) } > 2 < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleDigitClick ( 3 ) } > 3 < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleOperatorClick ( '+' ) } > + < / botão >
+        < / div >
+        < div  className = "linha" >
+          < botão  className = "btn"  onClick = { ( )  =>  handleDigitClick ( 0 ) } > 0 < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleDecimalClick ( ) } > . < / botão >
+          < botão  className = "btn"  onClick = { ( )  =>  handleEqualClick ( ) } > = < / botão >
+        < / div >
+      < / div >
+    < / div >
+  ) ;
+}
+
+exportar aplicativo padrão ;  
