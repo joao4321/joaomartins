8693efaa6226cb60037c210fc332e70511d033b0
joao4321 <89591695+joao4321@users.noreply.github.com>
2023-06-13 23:17:26 -0300

    Update atividade3.js

--- atividade3.js
+++ atividade3.js
@@ -1,10 +1,60 @@
-let n = prompt('Digite um número')
-resto = n % 2
-if(resto == 0)
-{
-    alert("Número par! ", n);
+import React, { useState } from 'react';
+import './Contador.css';
+import imagemHomem from './Imagens/homem.webp';
+import ImagemMulher from './Imagens/mulher.jpg';
+
+function ContadorPessoas() {
+	const [total, setTotal] = useState(0);
+	const [homens, setHomens] = useState(0);
+	const [mulheres, setMulheres] = useState(0);
+
+	const AddPerson = (gender) => {
+		if (gender === 'homens') {
+			setHomens(homens + 1);
+			setTotal(total + 1);
+		} else if (gender === 'mulheres') {
+			setMulheres(mulheres + 1);
+			setTotal(total + 1);
+		}
+	};
+
+	const RemovePerson = (gender) => {
+		if (gender === 'homens' && homens > 0) {
+			setHomens(homens - 1);
+			setTotal(total - 1);
+		} else if (gender === 'mulheres' && mulheres > 0) {
+			setMulheres(mulheres - 1);
+			setTotal(total - 1);
+		}
+	};
+
+	const Refresh = () => {
+		setTotal(0);
+		setHomens(0);
+		setMulheres(0);
+	};
+
+	const isNegative = total < 0;
+
+	return (
+		<div className='container'>
+			<h1>Total: {total}</h1>
+			{isNegative && <p style={{ color: 'red' }}>Número total de pessoas não pode ser negativo</p>}
+			<div class='buttons-container'>
+				<img class="img" src={imagemHomem} alt={'Homem'} />
+				<h2>Homens: {homens}</h2>
+				<button class="botao" onClick={() => AddPerson('homens')}>+</button>
+				<button class="botao" onClick={() => RemovePerson('homens')}>-</button>
+			</div>
+			<div class='buttons-container'>
+				<img class="img" src={ImagemMulher} alt={'Mulher'} />
+				<h2>Mulheres: {mulheres}</h2>
+				<button class="botao" onClick={() => AddPerson('mulheres')}>+</button>
+				<button class="botao" onClick={() => RemovePerson('mulheres')}>-</button>
+			</div>
+			<button className='refresh-button' onClick={Refresh}>Limpar</button>
+		</div>
+	);
 }
-else(resto != 0)
-{
-    alert("Número ímpar! ", n);
-}
+
+export default ContadorPessoas;
